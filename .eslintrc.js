1d0523c014fffbc695c2a6cf39ab7957889b268c
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-01-29 19:54:23 -0500

    linter config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'no-console': 0,
+  },
+}
