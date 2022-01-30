c929510ec87611d12863a83e849cba3cc65aa4d8
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-01-29 20:50:29 -0500

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,4 +3,9 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0)
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => (blogs.length === 0 ? null : blogs.reduce(
+  (max, blog) => (max.likes > blog.likes ? max : blog),
+  {},
+))
+
+module.exports = { dummy, totalLikes, favoriteBlog }
