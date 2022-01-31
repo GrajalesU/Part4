3bb1c322f83eb6232f53d602819417d558bc5680
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-01-31 17:00:38 -0500

    4.8

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGO_DB
+let MONGODB_URI = process.env.MONGO_DB
 const { PORT } = process.env
 
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGO_DB
+}
 module.exports = {
   MONGODB_URI,
   PORT,
