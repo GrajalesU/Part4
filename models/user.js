b828ea2f1fc3eec6372c8510400058bfa9488923
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-02-06 17:02:38 -0500

    put updated

--- models/user.js
+++ models/user.js
@@ -24,7 +24,6 @@ userSchema.plugin(uniqueValidator)
 userSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
     returnedObject._id = returnedObject._id.toString()
-    delete returnedObject._id
     delete returnedObject.__v
     delete returnedObject.passwordHash
   },
